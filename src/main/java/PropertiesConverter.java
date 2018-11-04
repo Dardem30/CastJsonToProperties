54b0d5f661ec97184519737182d05455a185779e
Roman <rlukashenko@cygno.com>
2018-11-04 20:39:32 +0300

    streams

--- src/main/java/PropertiesConverter.java
+++ src/main/java/PropertiesConverter.java
@@ -20,37 +20,36 @@ public class PropertiesConverter implements IConverter {
         final Properties properties = new Properties();
         properties.load(new FileInputStream(propertiesPath));
         final JSONObject jsonObject = new JSONObject(text);
+//        jsonObject.toMap().forEach((key, value) -> {
+//            if (value instanceof ArrayList) {
+//                ArrayList list =
+//                        (ArrayList) value;
+//                IntStream.range(0, list.size())
+//                        .forEach(index -> properties.put(key + "." + (index + 1), list.get(index)));
+//            } else if (value instanceof Map) {
+//                jsonObjectEntry(key, properties, (Map<String, Object>) value);
+//            } else {
+//                //  properties.put(key, String.valueOf(value));
+//            }
+//        });
         //todo make stream collect
-        jsonObject.toMap().forEach((key, value) -> {
-            if (value instanceof ArrayList) {
-                ArrayList list =
-                        (ArrayList) value;
-                IntStream.range(0, list.size())
-                        .forEach(index -> properties.put(key + "." + (index + 1), list.get(index)));
-            } else if (value instanceof Map) {
-                jsonObjectEntry(key, properties, (Map<String, Object>) value);
-            } else {
-                //  properties.put(key, String.valueOf(value));
-            }
-        });
         Stream<Map.Entry<String, Object>> stream = jsonObject.toMap().entrySet().stream();
-       /////// collect
-//        List<Map.Entry<String, Object>> list = stream.filter(entry -> !(entry.getValue() instanceof Map) && !(entry.getValue() instanceof ArrayList))
-//                .collect(Collectors.toList());
-//
-//        for (Map.Entry entry : list)
-//            properties.put(entry.getKey(), String.valueOf(entry.getValue()));
-        stream.filter(entry -> !(entry.getValue() instanceof Map) && !(entry.getValue() instanceof ArrayList))
-                .forEach(entry -> properties.put(entry.getKey(), String.valueOf(entry.getValue())));
-        stream.filter(entry -> entry instanceof ArrayList)
-                .forEach(entry -> {
-                    ArrayList list =
-                            (ArrayList) entry.getValue();
-                    IntStream.range(0, list.size())
-                            .forEach(index -> properties.put(entry.getKey() + "." + (index + 1), list.get(index)));
-                });
-        stream.filter(entry -> entry instanceof Map)
-                .forEach(entry -> jsonObjectEntry(entry.getKey(), properties, (Map<String, Object>) entry.getValue()));
+        properties.putAll(stream
+                .peek(entry -> {
+                    if (entry.getValue() instanceof Map)
+                        jsonObjectEntry(entry.getKey(), properties, (Map<String, Object>) entry.getValue());
+                })
+                .filter(entry -> !(entry.getValue() instanceof Map))
+                .peek(entry -> {
+                    if (entry.getValue() instanceof ArrayList) {
+                        ArrayList list =
+                                (ArrayList) entry.getValue();
+                        IntStream.range(0, list.size())
+                                .forEach(index -> properties.put(entry.getKey() + "." + (index + 1), list.get(index)));
+                    }
+                })
+                .filter(entry -> !(entry.getValue() instanceof ArrayList))
+                .collect(Collectors.toMap(val -> val.getKey(), val -> String.valueOf(val.getValue()))));
 
         FileWriter writer = new FileWriter(propertiesPath);
         properties.store(writer, null);
